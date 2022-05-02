3aea7cab71240a403d7d0f180e8c722b2a74c9de
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-05-02 17:26:56 +0200

    Added option base_href to the field content in create / edit form

--- src/app/forms/admin/wiki_pages/wiki_pages_form.php
+++ src/app/forms/admin/wiki_pages/wiki_pages_form.php
@@ -4,6 +4,7 @@ class WikiPagesForm extends AdminForm {
 	function set_up(){
 		$this->add_field("content", new MarkdownField([
 			"label" => _("Obsah"),
+			"base_href" => Atk14Url::BuildLink(["controller" => "wiki_pages", "action" => "index"]),
 		]));
 
 		$this->add_field("name",new RegexField('/^([A-Z][a-z0-9]*)+$/',[
